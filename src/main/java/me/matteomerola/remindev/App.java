00bb6d07c13f1b5a9baccd6a20473ec9c98fb919
Matteo Merola <mattmezza@gmail.com>
2017-03-24 17:15:38 +0100

    base structure

--- src/main/java/me/matteomerola/remindev/App.java
+++ src/main/java/me/matteomerola/remindev/App.java
@@ -1,13 +1,26 @@
 package me.matteomerola.remindev;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    public static final String QUIT = "q";
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("> ");
+        String query = scanner.next();
+        while (!query.equalsIgnoreCase(App.QUIT)) {
+            // simple echo
+            System.out.println(query);
+            
+            System.out.print("> ");
+            query = scanner.next();
+        }
+        System.out.println("Bye bye...");
     }
 }
